b69a7815e82967d2f14f545fea4fc594c26cb9ff
Adiel <adieltzubery94@gmail.com>
2023-01-05 21:09:51 +0200

    second branch test

--- base/views.py
+++ base/views.py
@@ -23,7 +23,7 @@ def loginUser(request):
 
         if user is not None:
             login(request, user)
-            return HttpResponse('user is logged in (github branch check)')
+            return HttpResponse('user is logged in (Hello World)')
 
     else:
         form = LoginUserForm()
