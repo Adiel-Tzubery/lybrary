1b1d1ddf33ea7fceff0db4e3929440fe3c1fcbca
Adiel <adieltzubery94@gmail.com>
2023-01-05 19:54:37 +0200

    coomit to check if work
    commit to check if work

--- base/views.py
+++ base/views.py
@@ -23,7 +23,7 @@ def loginUser(request):
 
         if user is not None:
             login(request, user)
-            return HttpResponse('user is logged is')
+            return HttpResponse('user is logged in (github chech)')
 
     else:
         form = LoginUserForm()
